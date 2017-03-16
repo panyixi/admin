15531c87cceb1cf9986a4a7118f1369ec2ed49ef
panyixi <229339722@qq.com>
2017-03-16 12:52:55 +0800

    modify home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <style scoped lang="scss">
 
 @import "./common/css/main.scss";
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin-top: 60px;
+}
+
+.router-link-active {
+    color: red;
+}
+
 .view {
     &.one {
         color: #333;
     }
     &.two {
         color: blue;
-    }
-    &.three {
-        color: red;
     }
 }
 
@@ -40,23 +49,6 @@
 
     <router-view class="view two" name="a"></router-view>
     <router-view class="view three" name="b"></router-view>
-
-    <!-- <img src="./common/img/logo.png">
-    <h1>{{msg}}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-        <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-        <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-        <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-        <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-        <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-        <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-        <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul> -->
 </div>
 
 </template>
@@ -67,7 +59,7 @@ export default {
     // name: 'app',
     data() {
         return {
-            msg: 'Welcome to Your Vue.js App'
+
         }
     },
     watch: {
